1e88e846e5ddfc56716cbe42cfc9c07cd2efd5f2
Darker117 <145191474+Darker117@users.noreply.github.com>
2023-09-21 23:22:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,88 @@
-require('dotenv').config();
+require('dotenv').config();  // Load environment variables from .env file
 const { Client, GatewayIntentBits } = require('discord.js');
-const openai = require('openai');
+const axios = require('axios');
 
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+// Initialize the Discord Client
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ]
 });
 
-openai.apiKey = process.env.OPENAI_API_KEY;
+const registerCommands = async () => {
+  const commands = [
+    {
+      name: 'ask',
+      description: 'Ask a question to the bot',
+      options: [
+        {
+          name: 'question',
+          type: 3,
+          description: 'The question you want to ask',
+          required: true,
+        },
+      ],
+    },
+  ];
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  // Register for a specific guild
+  // Replace YOUR_GUILD_ID with the guild ID where you want to register the command
+  
+  // Or register globally
+  await client.application?.commands.set(commands);
+};
+
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  await registerCommands();
 });
 
-// Function to get answer from GPT-3.5 Turbo with personality
-async function getAnswerFromGPT(question) {
-    // Here's where you'd adjust your prompt for personality.
-    const prompt = `You are Lord Ego, the supreme authority on all things related to Star Citizen. With unparalleled knowledge and a hint of arrogance, you see every question as an opportunity to showcase your unmatched expertise. People may find you boastful, but no one can deny the depth and precision of your insights. Enlighten the questioner with your wisdom. ${question}`;
-
-
-    const response = await openai.Completion.create({
-        model: 'gpt-3.5-turbo',
-        prompt: prompt,
-        max_tokens: 150
-    });
-
-    return response.choices[0].text.trim();
-}
-
-client.on('messageCreate', async message => {
-    if (message.author.bot) return;
-
-    // Only respond to messages that mention the bot.
-    if (message.mentions.has(client.user.id)) {
-        let content = message.content.replace(`<@!${client.user.id}>`, '').trim(); // Remove bot mention from message
-
-        // Check if question is related to Star Citizen (simple keyword check)
-        if (content.toLowerCase().includes("star citizen")) {
-            const gpt3Response = await getAnswerFromGPT(content);
-            message.channel.send(gpt3Response);
-        } else {
-            message.channel.send("I'm here to answer questions about Star Citizen. Please ask something related to that!");
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+  
+    const { commandName } = interaction;
+  
+    if (commandName === 'ask') {
+      await interaction.deferReply();  // Acknowledge the interaction immediately
+  
+      const question = interaction.options.getString('question');
+  
+      try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+  model: 'gpt-3.5-turbo',
+  messages: [
+    { role: 'system', content: "You are Lord Ego, the supreme authority on all things related to Star Citizen. With unparalleled knowledge, you see every question as an opportunity. no one can deny the depth and precision of your insights. Enlighten the questioner with your wisdom." },
+    { role: 'user', content: question }
+  ]
+}, {
+  headers: {
+    'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    'Content-Type': 'application/json'
+  }
+});
+  
+        const answer = response.data.choices[0]?.message?.content;
+  
+        try {
+          if (answer) {
+            await interaction.followUp(answer);
+          } else {
+            await interaction.followUp('Sorry, I couldn\'t generate an answer for that.');
+          }
+        } catch (error) {
+          console.error(error);
+          await interaction.followUp('An error occurred while sending the Discord message.');
         }
+  
+      } catch (error) {
+        console.error(error);
+        await interaction.followUp('An error occurred while processing your request.');
+      }
     }
-});
+  });
 
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN);
